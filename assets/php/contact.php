e0cea503359d00beb213ff8f060e5736666ec93d
Juanito2930 <jj956237@cameron.edu>
2025-08-21 23:16:14 -0500

    Update: contact form

---
+++ assets/php/contact.php
@@ -0,0 +1,31 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+$name= $_POST["name"];
+$email= $_POST["email"];
+$message= $_POST["message"];
+$subject= "KS Website Inquery";
+$txt = "You have received an e-mail from: " .$name.".\n\n\n\n".$message;
+
+
+if(mail(
+
+	//-----------------------------EDIT HERE-------------------------------------------------------------------------->
+  'cukappasigma1869@gmail.com', // To email address
+  $subject, // Subject
+  $txt, // Message body
+  "From: " .$email // Message headers
+)) {
+  echo "Email sent!";
+  
+} else {
+  $error = error_get_last();
+  echo "Failed to send email";
+  echo $error;
+}
+
+
+
+?>
